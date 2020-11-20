90b048456806b03bafef3d54f5358826c48c5a45
Brian Ealdwine <eode@eptitude.net>
2020-11-19 19:42:20 -0500

    Add pyserial as dependency

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/ccwienk/temper',
     py_modules=['temper'],
+    install_requires=['pyserial'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
