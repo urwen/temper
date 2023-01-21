3f6c888bb17bd1f3a3833c187ad2fc2e38ec6aff
Owen Brake <owen@moveparallel.com>
2023-01-20 19:47:38 -0800

    Fix TEMPerHUM Bug

--- temper.py
+++ temper.py
@@ -247,8 +247,8 @@ class USBRead(object):
       #Bytes 11-12 hold the external temp, divide by 100
       self._parse_bytes('external temperature', 10, 100.0, bytes, info, self.verbose)
       return info
-    if info['firmware'][:16] == 'TEMPerHUM_V3.9':
-      info['firmware'] = info['firmware'][:16]
+    if info['firmware'][:14] == 'TEMPerHUM_V3.9':
+      info['firmware'] = info['firmware'][:14]
       #Bytes 3-4 hold the device temp, divide by 100
       self._parse_bytes('internal temperature', 2, 100.0, bytes, info, self.verbose)
       #Bytes 11-12 hold the external temp, divide by 100
