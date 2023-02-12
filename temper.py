226ba27e576846f35b2bb6c2b5e11a6378a0088a
Bart Noordervliet <bart@mmvi.nl>
2023-02-12 20:14:32 +0100

    Add support for TEMPerF1.2 firmware

--- temper.py
+++ temper.py
@@ -200,7 +200,7 @@ class USBRead(object):
     info['hex_firmware'] = str(binascii.b2a_hex(firmware), 'latin-1')
     info['hex_data'] = str(binascii.b2a_hex(bytes), 'latin-1')
 
-    if info['firmware'][:10] in [ 'TEMPerF1.4', 'TEMPer1F1.' ]:
+    if info['firmware'][:10] in [ 'TEMPerF1.2', 'TEMPerF1.4', 'TEMPer1F1.' ]:
       info['firmware'] = info['firmware'][:10]
       self._parse_bytes('internal temperature', 2, 256.0, bytes, info)
       return info
